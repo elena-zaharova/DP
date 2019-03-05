2da63721af001a6072bd0797b4abcc850d738c90
elena-zaharova <elenazahar1998@gmail.com>
2019-03-05 21:20:45 +0300

    lab1

---
+++ ds-1-master/ds-1-master/src/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Frontend.Models;
+using System.Net.Http;
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(string data)
+        {
+            string id = null; 
+            id = await GetInfo(data);
+            Console.WriteLine("Value is: " + id); 
+            return Ok(id);
+        }
+
+        private static async Task<string> GetInfo(string data)
+        {
+            string url = "http://localhost:5001/api/values";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.PostAsJsonAsync(url, data);
+            response.EnsureSuccessStatusCode();        
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
